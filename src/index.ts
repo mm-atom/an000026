c4c9f631830d67e7cb6c7536d950fd5280fe4194
taoqf <tao_qiufeng@126.com>
2020-03-17 13:00:06 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+import an22 from '@mmstudio/an000022';
+import fetch from 'node-fetch';
 
-export default function an26(param1: string) {
+/**
+ * 是否包含违法文字
+ * @param headers
+ * @param text 要检测的文本内容
+ * @returns true 违法文字 false 正常文字
+ */
+export default async function check(text: string) {
+	const token = await an22();
+	const url = `https://api.weixin.qq.com/wxa/msg_sec_check?access_token=${token}`;
+	const str = JSON.stringify({ content: text });
+	const res = await fetch(url, {
+		body: str,
+		method: 'POST'
+	});
+	if (res.status > 0 && res.status < 300) {
+		const result = await res.json() as {
+			errcode: number;
+			errMsg: string;
+		};
+		return result.errcode === 87014;
+	} else {
+		throw new Error(res.statusText);
+	}
 }
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,9 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('微信-是否为违法文字', async (t) => {
+	const flag = await a(`特3456书yuuo莞6543李zxcz蒜7782法fgnv级`);
+	t.is(flag, true)
+	const flag1 = await a(`测试`);
+	t.is(flag1, false)
 });
